08a8b3d226b14f85149fc657c3f4bbd9b486cef4
Dilum Harshana <dilum.harshana@itelasoft.com>
2025-01-29 00:56:39 +0530

    imporovement - added open ai integration

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
 from sharpapi import SharpApiService
 from flask_cors import CORS  # Import CORS
+from openai import OpenAI
+import os
 
 app = Flask(__name__)
 sharp_api = SharpApiService(api_key="UvizKo42mpx6ieSVOv3cdM4WidlyKmbyYCAJVnYH")
 CORS(app)
+
+
+openaiKey = "sk-proj-JNxacNZax8A02p_AHCE75Rsg_W3XaOmr-bctysHnrsp26BmosEYjywJlTyrVjwMIDP9up1DmYAT3BlbkFJkpaIsl - \
+    fkAKt6-D5tovaSmZUHQD3bdnaRg6wlAr05ZKSdB3ZTOtACoYlcQ1bZr9C_OgJG7oKcA"
 
 
 @app.route('/upload', methods=['POST'])
@@ -30,12 +36,26 @@ def upload_file():
         # Fetch and return the parsed resume data
         parsed_resume = sharp_api.fetch_results(status_url)
         result = parsed_resume.get_result_json()
-
-        print(result)
-
         return ({"message": "File processed successfully", "data": result})
     except Exception as e:
+        print(e)
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        max_tokens=50,
+        n=1,
+        temperature=0,
+        messages=[
+            {"role": "user", "content": "hello"}
+        ]
+    )
+
+    print(response)
 
 
 if __name__ == '__main__':
